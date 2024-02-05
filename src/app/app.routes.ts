77a0e4320de3be9c24aac90d87eb8f9f0a62c783
StingrayFG <54187585+StingrayFG@users.noreply.github.com>
2024-02-05 04:04:09 +0300

    Add redirect page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './homePage/homePage.component'
+import { RedirectPageComponent } from './redirectPage/redirectPage.component'
 
 export const routes: Routes = [
   { path: '', component: HomePageComponent },
+  { path: ':uuid', component: RedirectPageComponent },
 ];
---
+++ src/app/redirectPage/redirectPage.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
+import axios from 'axios';
+
+@Component({
+  selector: 'app-page',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './redirectPage.component.html',
+  styleUrl: './redirectPage.component.scss'
+})
+
+export class RedirectPageComponent {
+  message = '...';
+
+  constructor(private route: ActivatedRoute) { }
+
+  async ngOnInit() {
+    await axios.get('http://localhost:5200/record/get/' + this.route.snapshot.paramMap.get('uuid'))
+    .then(res => {
+      window.location.href = res.data.originalLink;
+    })
+    .catch(err => {
+      this.message = '404 Not Found'
+    })
+  }
+}
